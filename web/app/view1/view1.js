0c64c4e410b32399051258c41bf3111a5f5e763e
biggiejr <jrmacej@gmail.com>
2015-12-02 18:28:41 +0100

    Front end finished
    Search completed

--- web/app/view1/view1.js
+++ web/app/view1/view1.js
@@ -1,15 +1,54 @@
-'use strict';
-
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'app/view1/view1.html',
-    controller: 'View1Ctrl',
-    controllerAs : 'ctrl'
-  });
-}])
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view1', {
+                    templateUrl: 'app/view1/view1.html',
+                    controller: 'View1Ctrl',
+                    controllerAs: 'ctrl'
+                });
+            }])
 
-.controller('View1Ctrl', ["InfoFactory","InfoService",function(InfoFactory,InfoService) {
-        
-}]);
+
+        .controller('View1Ctrl', ["$scope", "$http", function ($scope, $http) {
+                $http.get('api/flightinfo')
+                        .success(function (data, status, headers, config) {
+                            $scope.data = data;
+                        })
+                        .error(function (data, status, headers, config) {
+
+                        });
+
+
+                $scope.airport = [{
+                        id: 1,
+                        label: 'Copenhagen',
+                        name: 'CPH'
+                    }, {
+                        id: 2,
+                        label: 'Berlin',
+                        name: 'SXF'
+                    }];
+                $scope.output = {};
+
+                $scope.getFlight = function () {
+                    return $http({method: 'GET', url: '/3rdSemesterProject/api/flightinfo/' + $scope.chosenAirport.name + '/' + $scope.date.toISOString() + '/' + $scope.tickets,
+                        contentType: "application/json"}).success(function (data, status, headers, config) {
+
+                        $scope.output = data;
+
+
+                    }).
+                            error(function (data, status, headers, config) {
+
+                            });
+                };
+
+
+
+
+            }]
+                );
+
+
+
+
